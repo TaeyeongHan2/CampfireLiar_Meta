c2a8b6e935bda32b868c3e50895dbdbc61b94763
TaeyeongHan <dianspace53@gmail.com>
2025-05-20 20:06:00 +0900

    닉네임 입력 및 방 입력 후 접속 버튼 눌렀을 때 LobbyScene으로 전환되는 것 까지 구현(Event System 중복 오류 수정 예정)

---
+++ Assets/Scripts/LobbyManager.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Fusion;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviour
+{
+    public NetworkPrefabRef normalPlayerPrefab;
+    public NetworkPrefabRef chiefPlayerPrefab;
+    
+    
+    
+}
