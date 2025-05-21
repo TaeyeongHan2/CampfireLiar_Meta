98d019759ffd05c28c0af9cda9b03c5fd753ef4f
TaeyeongHan <dianspace53@gmail.com>
2025-05-21 10:21:30 +0900

    선아꺼 로비 씬 ui 풀 전 커밋

--- Assets/Scripts/FusionSession.cs
+++ Assets/Scripts/FusionSession.cs
@@ -1,115 +1,115 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Fusion;
-using Fusion.Sockets;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class FusionSession : MonoBehaviour, INetworkRunnerCallbacks
-{
-    public NetworkRunner runnerPrefab;
-    
-    [SerializeField] [ScenePath] private string loginScene;
-    [SerializeField] [ScenePath] private string lobbyScene;
-    [SerializeField] [ScenePath] private string demoNight;
-
-    public NetworkRunner Runner { get; private set; }
-    
-    
-    
-    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnInput(NetworkRunner runner, NetworkInput input)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnConnectedToServer(NetworkRunner runner)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnSceneLoadDone(NetworkRunner runner)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void OnSceneLoadStart(NetworkRunner runner)
-    {
-        throw new NotImplementedException();
-    }
-}
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using Fusion;
+// using Fusion.Sockets;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+//
+// public class FusionSession : MonoBehaviour, INetworkRunnerCallbacks
+// {
+//     public NetworkRunner runnerPrefab;
+//     
+//     [SerializeField] [ScenePath] private string loginScene;
+//     [SerializeField] [ScenePath] private string lobbyScene;
+//     [SerializeField] [ScenePath] private string demoNight;
+//
+//     public NetworkRunner Runner { get; private set; }
+//     
+//     
+//     
+//     public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnInput(NetworkRunner runner, NetworkInput input)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnConnectedToServer(NetworkRunner runner)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnSceneLoadDone(NetworkRunner runner)
+//     {
+//         throw new NotImplementedException();
+//     }
+//
+//     public void OnSceneLoadStart(NetworkRunner runner)
+//     {
+//         throw new NotImplementedException();
+//     }
+// }
