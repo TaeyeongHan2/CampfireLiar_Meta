6be6129c09db0596c9b8d3115544cf17d3d921b8
TaeyeongHan <dianspace53@gmail.com>
2025-05-21 14:21:16 +0900

    선아가 만든 로비씬 UI 및 로그인 씬 로고 실제 실행 씬으로 옮김

--- Assets/Scripts/NetworkPlayerSpawner.cs
+++ Assets/Scripts/NetworkPlayerSpawner.cs
@@ -1,16 +1,32 @@
 using Fusion;
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Serialization;
 
 public class NetworkPlayerSpawner : SimulationBehaviour, IPlayerJoined
 {
-    public GameObject playerPrefab;
+    //로비에 생성되는 플레이어 UI 프리팹
+    public GameObject lobbyPlayerUIPrefab;
+    //게임 씬에 생성되는 플레이어 3D 오브젝트 프리팹
+    public GameObject playerPrefab3D;
 
+    //로비에서 호출
+    public void SpawnLobbyPlayerUI(PlayerRef player, NetworkRunner runner)
+    {
+        if(Object.HasStateAuthority == false) return;
+        
+        //단발성이 rpc고
+        //값 자체가 동기화되는게 값이 변결될 때 계속 알려주는거 networked라는 attribute로 동기화
+        //내부적으로 동기화할 때 ui 도 갱신
+        
+    }
+    
+    
     public void PlayerJoined(PlayerRef player)
     {
         if (player == Runner.LocalPlayer)
         {
-            Runner.Spawn(playerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            Runner.Spawn(playerPrefab3D, new Vector3(0, 0, 0), Quaternion.identity);
         }
     }
 }
